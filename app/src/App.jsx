6bf3777abc5e89086b79fc401f57365fb700266f
Ivan Castro <ivanrcastro773@gmail.com>
2021-12-08 18:59:09 -0300

    [make/card/footer]: hice una card y el footer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom'
-
+import {Routes, Route,Link} from "react-router-dom";
 import './App.css';
+import Footer from "./components/footer/Footer";
+import Character from "./components/main/section/character/Character";
+import Nav from './components/nav/Nav';
 
 function App() {
   let logged = true;
@@ -29,64 +31,70 @@ function App() {
         </ul> */}
         <nav className="navbar sticky-top navbar-expand-lg bg-main-dark color-light">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">Star Wars</ Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <Link className="navbar-brand" to="/">
+              Star Wars
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="offcanvas offcanvas-end bg-main-dark" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div
+              className="offcanvas offcanvas-end bg-main-dark"
+              tabIndex="-1"
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+            >
               <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                  Offcanvas
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close text-reset"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
               </div>
-              <div className="offcanvas-body">
+              <div className="offcanvas-body ">
                 <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <i class="fab fa-jedi-order fa-5x icon-starwars"></i>
                   <li className="nav-item">
-                    <Link className="nav-link active" to="/movies">movies</Link>
+                    <Link className="nav-link active" to="/movies">
+                      movies
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link" to="/characters">characters</ Link>
+                    <Link className="nav-link" to="/characters">
+                      characters
+                    </Link>
                   </li>
                 </ul>
-                <div className="session-actions">
-                  {
-                    logged ? (
-                      <button className="btn btn-danger">Logout</button>
-                    ) : (
-                      <button className="btn btn-primary">Login</button>
-                    )
-                  }
-                </div>
+                {<div className="session-actions">
+                  {logged ? (
+                    <button className="btn btn-danger">Logout</button>
+                  ) : (
+                    <button className="btn btn-primary">Login</button>
+                  )}
+                </div>}
               </div>
             </div>
           </div>
         </nav>
       </div>
       <section>
-        <h2>carrousel</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore impedit placeat illum natus cum delectus odit quae sit quo fuga, exercitationem architecto distinctio iste, repudiandae voluptates nulla saepe! Reprehenderit!</p>
+        <Routes>
+          <Route index path="/"/>
+          <Route path="/movies" element={<Nav/>} />
+          <Route path="/characters"  />
+        </Routes>
       </section>
-      <section>
-        <h2>sub carrousel</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores sint eos reiciendis, ullam aliquid incidunt necessitatibus, obcaecati impedit quas culpa eveniet aliquam a neque amet cumque autem explicabo sapiente voluptate!</p>
-      </section>
-      <section>
-        <h2>cards</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti corporis sapiente in ullam velit doloribus ex rerum possimus est, laborum provident ratione, neque autem nemo tempora fugiat! Maxime, omnis necessitatibus.</p>
-      </section>
-      <section>
-        <h2>movies</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate nobis nisi vel. Inventore quidem minima officia repellendus pariatur beatae repellat earum consequatur expedita. Quasi autem, reprehenderit quam eius perferendis animi.</p>
-      </section>
-      <main>
-        <h2>hola</h2>
-        <p>lorem ipsum</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, repellendus! Repellendus soluta, hic vitae voluptatum id corporis incidunt. Excepturi doloribus voluptates obcaecati eum repellendus amet tempore animi esse, maxime nemo.</p>
-      </main>
-      <footer>
-        <h2>footer</h2>
-        <p>lorem ipsum</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae magnam iste laudantium dolore, rerum inventore illo necessitatibus. Atque, nisi culpa nobis ipsam autem ipsum maxime. Eaque maxime porro dolor aliquam!</p>
-      </footer>
+      <Character/>
+      <Footer/> 
     </div>
   );
 }
---
+++ app/src/components/main/section/character/Character.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './character.css';
+const Character = () => {
+  return (
+      <>
+    <div className="container-cards-character">
+      <div>
+        <div className="card-character">
+          <div className="text-character">
+            <h3 className="title-character">Luke Skywalker</h3>
+            <ul className="text-description">
+              <li>altura</li>
+              <li>masa</li>
+            </ul>
+            <p className="title-character">planeta</p>
+          </div>
+        </div>
+      </div>
+    <div>
+        <button className="btn-edit-character">EDITAR</button>
+    </div>
+    </div>
+    </>
+  );
+};
+
+export default Character;
