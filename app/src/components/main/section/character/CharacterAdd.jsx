39f37e2f4cc5d28179f7cb7898f026174ebdcf6f
Ivan Castro <ivanrcastro773@gmail.com>
2021-12-13 13:18:52 -0300

    [updated/component]: characterAdd

--- app/src/components/main/section/character/CharacterAdd.jsx
+++ app/src/components/main/section/character/CharacterAdd.jsx
@@ -5,25 +5,17 @@ import CharacterFormEdit from './CharacterFormEdit';
 
 const CharacterAdd = ({character}) => {
   const [info, setInfo] = useState(true);
-  const [addImg,setAddImg] = useState(true);
 
-  const btnAddImage=()=>{
-      return(
-          <div className='probando'>
-              <button>Subir Imagen</button>
-          </div>
-      )
-  }
   return (
     <>
       <div className="container-card-chac-add">
         <div className="card-box-character">
           <div className="img-character">
-            {addImg ? btnAddImage() : <img
+            <img
               src="img/characters/portrait.png"
               className="img-style-chac"
               alt=""
-            />}
+            />
           </div>
           <CharacterFormEdit setInfo={setInfo} />
         </div>
