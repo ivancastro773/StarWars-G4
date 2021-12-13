0c24f91b6a33c1282e0ac6dd288dbd04e035b62d
MartinH99 <martinhernandezmdp@gmail.com>
2021-12-13 16:48:48 -0300

    Commit 2{

--- app/src/components/main/section/card/card.jsx
+++ app/src/components/main/section/card/card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './card.css';
 
 const card = () => {
@@ -7,7 +8,7 @@ const card = () => {
         <h4>Meet our characters</h4>
 
         <p>Are you ready to meet the most famous characters in the filmographic world and all their characteristics?</p>
-        <p>Clicking <a href="http://localhost:3000/characters" target="_blank">here</a> you can discover what we have prepared for you...</p>
+        <p>Clicking <button><Link to = "characters"> here</Link></button> you can discover what we have prepared for you...</p>
     </div>
   );
 };
