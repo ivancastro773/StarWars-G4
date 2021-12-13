fde614b4140323c5631924dbde54a8e1618ecbaa
Ivan Castro <ivanrcastro773@gmail.com>
2021-12-13 12:33:11 -0300

    probando

--- app/src/components/main/section/character/CharacterAdd.jsx
+++ app/src/components/main/section/character/CharacterAdd.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
+//components
+import CharacterFormEdit from './CharacterFormEdit';
 
-const CharacterAdd =()=>{
-    return (
-        <div>
-            HOLA AGREGAR
+
+const CharacterAdd = ({character}) => {
+  const [info, setInfo] = useState(true);
+  const [addImg,setAddImg] = useState(true);
+
+  const btnAddImage=()=>{
+      return(
+          <div className='probando'>
+              <button>Subir Imagen</button>
+          </div>
+      )
+  }
+  return (
+    <>
+      <div className="container-card-chac-add">
+        <div className="card-box-character">
+          <div className="img-character">
+            {addImg ? btnAddImage() : <img
+              src="img/characters/portrait.png"
+              className="img-style-chac"
+              alt=""
+            />}
+          </div>
+          <CharacterFormEdit setInfo={setInfo} />
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default CharacterAdd;
